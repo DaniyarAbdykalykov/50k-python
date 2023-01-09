89399f3c687f48cde903b016b37e0d7496184191
Daniyar Abdykalykov <dzhalilovich7@gmail.com>
2023-01-09 18:00:58 +0300

    first commit

---
+++ lesson2/task3.py
@@ -0,0 +1,12 @@
+name = 'Абдыкалыков Данияр Джалилович;'
+birthday = '3 февраля 1997 года;'
+heigth = '182 см;'
+hobby = 'футбол, сноубординг, туризм;'
+favoriteMovie = '"Любовь. Смерть. Роботы"'
+
+print('1', name, 
+    '\n2.', birthday,
+    '\n3.', heigth,
+    '\n4.', hobby,
+    '\n5.', favoriteMovie
+)
