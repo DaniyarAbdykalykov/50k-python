582fe115248735dc1af6cab13781dff3b85bf7bf
Daniyar Abdykalykov <dzhalilovich7@gmail.com>
2023-01-13 09:45:20 +0300

    finish lesson 6

---
+++ lesson6/equal.py
@@ -0,0 +1,9 @@
+a = 43
+b = a + 3
+print(a, b)
+
+a = a + 1 # 44
+print(a)
+
+b = b - 1
+print(b)
---
+++ lesson6/operations.py
@@ -0,0 +1,26 @@
+num = 54
+num += 1
+print(num)
+
+num += 10
+print(num)
+
+d = 23
+d -= 15
+print(d)
+
+d *= 3
+print(d)
+
+d /= 6
+print(d)
+
+d **= 2
+print(d)
+
+d //= 5
+print(d)
+
+d = 17
+d %= 4
+print(d)
