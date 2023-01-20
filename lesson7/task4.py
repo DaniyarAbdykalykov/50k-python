a4d75bc9e3474a4f716d2abd899efd157a3f1fb0
Daniyar Abdykalykov <dzhalilovich7@gmail.com>
2023-01-20 10:07:26 +0300

    completed lesson 7

--- lesson7/task4.py
+++ lesson7/task4.py
@@ -1,5 +1,19 @@
 scholarship = 50
 
 GPA = float(input('Введите Ваш средний балл за все предметы: '))
-painting = lower(input('Если вы посещали рисование, напишите "да": '))
-sport = lower(input('Если вы посещали спортивные секции, напишите "да": '))
+
+additional_sections = int(input('В скольких кружках Вы состоите? '))
+
+volunteer = input('Если Вы занимались волонтерской деятельностью, напишите "да": ')
+volunteer = volunteer.lower()
+
+if GPA > 3.5:
+    scholarship += 100
+
+if additional_sections in (1, 2, 3, 4, 5, 6, 7, 8, 9):
+    scholarship += additional_sections * 30
+
+if volunteer == 'да':
+    scholarship += 50
+
+print(f'Ваша стипендия составляет {scholarship}$')
