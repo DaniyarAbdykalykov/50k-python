bc22d1f6cd0e8cab377df94bf308561d090d7aa7
Daniyar Andykalykov (laptop) <dzhalilovich7@gmail.com>
2023-02-23 19:24:08 +0300

    started 15 lesson

---
+++ lesson15/task2.py
@@ -0,0 +1,10 @@
+numbs_tuple = ()
+
+numb = int(input("Введите число: "))
+
+while numb > 0:
+    if numb not in numbs_tuple:
+        numbs_tuple = numbs_tuple + (numb,)
+    numb = int(input("Введите число: "))
+
+print(sorted(numbs_tuple))
---
+++ lesson15/task3.py
@@ -0,0 +1,7 @@
+numb = int(input("Введите число: "))
+count = 0
+my_set = []
+
+while count < numb:
+    string = set(input("Введите строку: "))
+    my_set.append(string)
