9d0a5b20f9998bdabbb3dedeb889fce0e3182fff
Daniyar Abdykalykov <dzhalilovich7@gmail.com>
2023-02-24 09:35:52 +0300

    ended 15 lesson

--- lesson15/task2.py
+++ lesson15/task2.py
@@ -4,7 +4,7 @@ numb = int(input("Введите число: "))
 
 while numb > 0:
     if numb not in numbs_tuple:
-        numbs_tuple = numbs_tuple + (numb,)
+        numbs_tuple += (numb,)
     numb = int(input("Введите число: "))
 
 print(sorted(numbs_tuple))
--- lesson15/task3.py
+++ lesson15/task3.py
@@ -3,5 +3,7 @@ count = 0
 my_set = []
 
 while count < numb:
-    string = set(input("Введите строку: "))
-    my_set.append(string)
+    inp_string = sorted(set(input("Введите строку: ")))
+    my_set.append(inp_string)
+    count += 1
+print(my_set)
